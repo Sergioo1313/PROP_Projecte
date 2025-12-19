af287b64b16675eee2fe308e020a9ecd45682c88
Sxnter <edgarsantervas@gmail.com>
2025-12-19 11:11:05 +0100

    projecte base

---
+++ Oust/src/edu/upc/epsevg/prop/oust/Game.java
@@ -0,0 +1,56 @@
+package edu.upc.epsevg.prop.oust;
+
+import edu.upc.epsevg.prop.oust.players.HumanPlayer;
+import edu.upc.epsevg.prop.oust.players.RandomPlayer;
+
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Oust: el joc de taula.
+ * @author Bernat
+ */
+public class Game {
+    /**
+     * @param args no usats
+     */
+    public static void main(String[] args) { 
+         
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                
+                //---------------------------------------------
+                // Un parell de tontets jugant 
+                //---------------------------------------------
+                //IPlayer player1 = new RandomPlayer("Asterix");
+                //IPlayer player2 = new RandomPlayer("Obelix");
+                
+                
+                
+                //---------------------------------------------
+                // Deixem el tontet en mans d'una mala persona
+                //---------------------------------------------
+                //IPlayer player1 = new RandomPlayer("Asterix");
+                //IPlayer player2 = new MalaOustiaPlayer();
+                
+                
+                //---------------------------------------------
+                // Enjoy!
+                //---------------------------------------------
+                IPlayer player1 = new MalaOustiaPlayer();
+                IPlayer player2 = new HumanPlayer("aaa");
+                                
+                
+                //---------------------------------------------
+                // Customitzeu els paràmetres
+                //---------------------------------------------
+                int midaCostat = 5;
+                int timeoutEnSegons = 3;
+                boolean pauseEnAutomatic = false;
+                
+                new Board(player1 , player2, midaCostat /*mida*/,  timeoutEnSegons/*s timeout*/, pauseEnAutomatic);
+             }
+        });
+    }
+}
